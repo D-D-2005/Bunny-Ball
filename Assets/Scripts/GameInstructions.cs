0570a268939066289f15d3758b3316947ca8dc7b
D-D-2005 <ddelacruz@aoiths.org>
2023-01-09 13:57:26 -0500

    Initial commit

---
+++ Assets/Scripts/GameInstructions.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInstructions : MonoBehaviour
+{
+    string gameinstructions = "Move the player by using the W,A,S and D keys ";
+    // Start is called before the first frame update
+    void Start()
+    {
+         Debug.Log("Move the player by using the W,A,S and D keys ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+}
